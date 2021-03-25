1bf2dc9133577b428f84d983fa544b425080b184
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-25 07:15:45 -0300

    feat(test): teste unitário do componente rocket

--- src/app/rocket/rocket.component.spec.ts
+++ src/app/rocket/rocket.component.spec.ts
@@ -3,6 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RocketComponent } from './rocket.component';
 
 describe('RocketComponent', () => {
+    const title = 'Playground App';
+
     let component: RocketComponent;
     let fixture: ComponentFixture<RocketComponent>;
 
@@ -15,10 +17,17 @@ describe('RocketComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(RocketComponent);
         component = fixture.componentInstance;
+        component.title = title;
+
         fixture.detectChanges();
     });
 
-    it('should create', () => {
+    it('should create the rocket component', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should render the title in span', () => {
+        const compiled = fixture.nativeElement;
+        expect(compiled.querySelector('span').textContent).toContain(`${title} is running!`);
     });
 });
