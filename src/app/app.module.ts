dcb8bd2782a3d3393ec71a188c836184e718152a
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-25 10:26:43 -0300

    feat(card-link): criado componente card link
    criado componente para exibição dos links através de cards
    
    Fixes #1234

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardLinkComponent } from './cards/card-link/card-link.component';
 import { CardRocketComponent } from './cards/card-rocket/card-rocket.component';
 
 @NgModule({
     bootstrap: [AppComponent],
     declarations: [
         AppComponent,
+        CardLinkComponent,
         CardRocketComponent
     ],
     imports: [BrowserModule],
